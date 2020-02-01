a40e970fd6ec7cc4d43f7dac9c72bc65b8223d3d
teruya.sawamura <t.s.16125@gmail.com>
2020-02-01 08:54:12 +0000

    user signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# (1..10).each do |number|
+#  User.create(name: 'taro ' + number.to_s, email: 'TaRo'+ number.to_s + '@TechAcademy.jp', password: 'taro1234'+ number.to_s)
+# end
+
+# User.create!(name: 'taro 12', email: 'TaRo@TechAcademy.jp', password: 'taro123412')
+# User.create(name: 'taro 2', email: 'TaRo@TechAcademy.jp', password: 'taro1234')
+# User.create(name: 'taro 3', email: 'TaRo@TechAcademy.jp', password: 'taro1234')
+# User.create(name: 'taro 4', email: 'TaRo@TechAcademy.jp', password: 'taro1234')
+# User.create(name: 'taro 5', email: 'TaRo@TechAcademy.jp', password: 'taro1234')
