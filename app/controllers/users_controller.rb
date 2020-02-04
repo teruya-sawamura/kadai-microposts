f543208d541b395044c0a01c404938632ebf6035
teruya.sawamura <t.s.16125@gmail.com>
2020-02-04 11:48:24 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    count(@user)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    # トップページのルーティング
     root to: 'toppages#index'
     
+    # ログイン用ルーティング
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
     
+    # ユーザ登録用ルーティング
     get 'signup', to: 'users#new'
+    
+    
     resources :users, only: [:index, :show, :new, :create]
+    
+    resources :microposts, only: [:create, :destroy]
 end
